401b7911486fb999c2f39d203e16069ae9a4c611
Eduardo-jorge <cuasar206@gmail.com>
2026-05-25 11:46:13 -0300

    Agregando mod y del

--- app/app.py
+++ app/app.py
@@ -56,6 +56,7 @@ def create_user():
     db.session.commit()
     
     return jsonify(new_user.to_dict()), 201
+
 # ==========================================
 # COMPLETANDO EL ABM DE USUARIOS (Requisito del TP)
 # ==========================================
